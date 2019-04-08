7e6c6eb2c73c618683996dfb623e5aafbbd2f878
Gaurav.Khanna <iamgauravkhanna@gmail.com>
2019-04-08 18:48:11 +0530

    New Fixes

---
+++ pythonExamples/data-types/data-types.py
@@ -0,0 +1,23 @@
+a=2.45
+
+print(type(a))
+
+b='gaurav'
+
+print(type(b))
+
+c=7+4j
+
+print(type(c))
+
+booleanF = 8 > 5
+
+print(type(booleanF))
+
+int(booleanF)
+
+print(type(booleanF))
+
+listObj = (43,45,76,87)
+
+print(type(listObj))
---
+++ pythonExamples/operator/operators.py
@@ -0,0 +1,9 @@
+a = 3
+
+a += 2
+
+print(a)
+
+a = -a
+
+print(a)
