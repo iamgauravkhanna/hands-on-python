31538e7034f6511fa8cc69916193f24ac5c22b71
iamgauravkhanna <iamgauravkhanna>
2019-09-02 18:46:11 +0530

    new examples

---
+++ pythonExamples/comments/comments.py
@@ -0,0 +1,7 @@
+"""
+
+This is multi line comments
+
+"""
+
+print('Hello Gaurav !')
---
+++ pythonExamples/functions/function-example-1.py
@@ -0,0 +1,7 @@
+def printDetails(name):
+    print("Printing Details for " + name)
+
+
+printDetails("Gaurav")
+print("")
+printDetails("Khanna")
