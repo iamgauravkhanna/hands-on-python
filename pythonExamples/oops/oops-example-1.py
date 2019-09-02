dfc40308a86bdfece6cee14ba2a59e213aa53054
iamgauravkhanna <iamgauravkhanna>
2019-09-02 17:31:32 +0530

    New Fixes

---
+++ pythonExamples/oops/oops-example-1.py
@@ -0,0 +1,16 @@
+# https://stackabuse.com/object-oriented-programming-in-python/
+
+# Creates class Car
+class Car:
+
+    # create class attributes
+    name = "c200"
+    make = "mercedez"
+    model = 2008
+
+    # create class methods
+    def start(self):
+        print ("Engine started")
+
+    def stop(self):
+        print ("Engine switched off")
