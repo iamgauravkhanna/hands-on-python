0fc1fdf371e1c5ace051d76074160c2c014faae9
Gaurav Khanna <iamgauravkhanna@gmail.com>
2020-11-25 16:17:18 +0530

    changes

--- pythonExamples/025-excel/WorkingWithExcel.py
+++ pythonExamples/025-excel/WorkingWithExcel.py
@@ -1,7 +1,6 @@
 __author__ = 'Gaurav.Khanna'
 
 from openpyxl import load_workbook
-
 import os.path
 
 wb = load_workbook(r'C:\GK\Selenium\PHIX_Trunk\assets\prod\tests\Broker.xlsx', "r")
--- pythonExamples/025-excel/excel-example-2.py
+++
@@ -1,5 +0,0 @@
-import openpyxl
-my_wb = openpyxl.Workbook()
-my_sheet = my_wb.active
-my_sheet_title = my_sheet.title
-print("My sheet title: " + my_sheet_title)
