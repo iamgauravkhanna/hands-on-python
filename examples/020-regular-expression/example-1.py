42eadf9c65f54d043c89b1f9dcd6c2a194ae6d06
Gaurav Khanna <gaurav.khanna@github.com>
2024-01-22 13:54:15 +0530

    changes-2024-01-22-13-54-15

---
+++ examples/020-regular-expression/example-1.py
@@ -0,0 +1,15 @@
+import re
+
+def is_valid_email(email):
+    # Define the regular expression pattern for a basic email validation
+    email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    
+    # Check if the input string matches the pattern
+    return bool(email_pattern.match(email))
+
+email_address = "user@example.com"
+
+if is_valid_email(email_address):
+    print(f"{email_address} is a valid email address.")
+else:
+    print(f"{email_address} is not a valid email address.")
---
+++ examples/021-files-input-output/example-1.py
@@ -0,0 +1,6 @@
+#https://www.tutorialspoint.com/python/python_files_io.htm
+
+fo = open("hello.txt", "wb")
+
+print('Name Of File ::' + fo.name)
+print('Mode Of File ::' + fo.mode)
