b40d55ace1a2c9e028dfd9fe119472bc449e5d10
Gaurav Khanna <gaurav.khanna@github.com>
2024-06-17 18:28:09 +0530

    changes-2024-06-17-18-28-09

---
+++ examples/100-projects/image-downloader/script-001.py
@@ -0,0 +1,21 @@
+import os, re, requests
+
+def ImageDownloader(url):
+
+    response = requests.get(url)
+    
+    text = response.text
+
+    p = r'<img.*?src="(.*?)"[^\>]+>'
+    img_addrs = re.findall(p, text)
+
+    for i in img_addrs:
+        os.system("wget {}".format(i))
+
+    return "DONE"
+
+print("Hey!! Welcome to the Image downloader...")
+
+link=input("Please enter the url from where you want to download the image..")
+
+ImageDownloader(link)
